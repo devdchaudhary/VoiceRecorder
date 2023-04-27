65a5ad634d1121c74030c46984783f0f1a7ad007
devanshuchaudhary <devanshu88@gmail.com>
2023-04-28 02:03:16 +0530

    Initial Commit

---
+++ CustomAudioRecorder/CustomAudioRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAudioRecorderApp.swift
+//  CustomAudioRecorder
+//
+//  Created by devdchaudhary on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomAudioRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
